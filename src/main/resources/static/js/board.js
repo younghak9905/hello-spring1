5ba7f0dd57f7d2fc5c1788ec33fbd353644baf92
52oesi <sjj7554@naver.com>
2023-02-09 17:45:11 +0900

    Merge remote-tracking branch 'origin/oesi' into oesi
    # Conflicts:
    #	src/main/java/com/example/demo/dto/AskRequestDto.java
    #	src/main/resources/templates/home.html
    #	src/main/resources/templates/questions/view.html

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -85,3 +85,66 @@ let index= {
         });
     }
 }
+
+    let markdownText = document.getElementById("wmd-input").value;
+
+    function marked(markdownText) {
+    return "";
+}
+
+    document.getElementById("wmd-input").innerHTML = marked(markdownText);
+    document.getElementById("bold-button").addEventListener("click", function(){
+    const editor = document.getElementById("wmd-input");
+    const selectedText = editor.value.substring(editor.selectionStart, editor.selectionEnd);
+    const newText = "**bold" + selectedText + "**";
+    editor.value = editor.value.substring(0, editor.selectionStart) + newText + editor.value.substring(editor.selectionEnd);
+});
+    document.getElementById("italic-button").addEventListener("click", function(){
+    const editor = document.getElementById("wmd-input");
+    const selectedText = editor.value.substring(editor.selectionStart, editor.selectionEnd);
+    const newText = "*italic" + selectedText + "*";
+    editor.value = editor.value.substring(0, editor.selectionStart) + newText + editor.value.substring(editor.selectionEnd);
+});
+    document.getElementById("link-button").addEventListener("click", function(){
+    let link = prompt("Enter the link here: ", "http://");
+    document.execCommand("createLink", false, link);
+});
+    document.getElementById("quote-button").addEventListener("click", function(){
+    const editor = document.getElementById("wmd-input");
+    const selectedText = editor.value.substring(editor.selectionStart, editor.selectionEnd);
+    const newText = ">blockquotes" + selectedText;
+    editor.value = editor.value.substring(0, editor.selectionStart) + newText + editor.value.substring(editor.selectionEnd);
+});
+    document.getElementById("image-button").addEventListener("click", function() {
+    const editor = document.getElementById("wmd-input");
+    const imul = prompt("Enter the image URL: ");
+    const newText = "![image](" + imul + ")";
+    editor.value = editor.value.substring(0, editor.selectionStart) + newText + editor.value.substring(editor.selectionEnd);
+});
+    document.getElementById("code-button").addEventListener("click", function() {
+    const editor = document.getElementById("wmd-input");
+    const selectedText = editor.value.substring(editor.selectionStart, editor.selectionEnd);
+    const newText = "` <code> " + selectedText + "`";
+    editor.value = editor.value.substring(0, editor.selectionStart) + newText + editor.value.substring(editor.selectionEnd);
+});
+
+    document.getElementById("reply-submit").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let markdownText = document.getElementById("wmd-input").value;
+
+    function marked(markdownText) {
+    return "";
+}
+
+    document.getElementById("wmd-input").innerHTML = marked(markdownText);
+    // form submit
+    this.submit();
+});
+
+
+
+
+
+
+
+
