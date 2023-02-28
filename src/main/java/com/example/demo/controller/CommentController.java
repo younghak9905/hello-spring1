a5f403dead4b5646112573cbac8a04b81d2688a1
younghak9905 <102720771+younghak9905@users.noreply.github.com>
2023-02-28 15:28:59 +0900

    Update CommentController.java

--- src/main/java/com/example/demo/controller/CommentController.java
+++ src/main/java/com/example/demo/controller/CommentController.java
@@ -13,6 +13,8 @@ import com.example.demo.service.CommentService;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -32,47 +34,97 @@ AskRepository askRepository;
     private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
     //test writer_no
-    private Long writer_no = 1L;
+
     @PostMapping("/comment/{no}")
     public String save(CommentRequestDto dto,@PathVariable("no") Long no){
-    Optional<Member> member =memberRepository.findByNo(writer_no);
-    dto.setWriter(member.get());
-        commentService.save(dto,no);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        if(!id.equals("anonymousUser")) {
 
-
-        return "redirect:/questions/"+no;
+            Member loginMember = memberRepository.findById(id);
+            dto.setWriter(loginMember);
+            commentService.save(dto, no);
+            return "redirect:/questions/" + no;
+        }
+        return "redirect:/members/login";
     }
 
 
 
     @PostMapping("/comment/{no}/reply")
     public String reply(CommentRequestDto dto,@PathVariable("no") Long no){
-        Optional<Member> member =memberRepository.findByNo(writer_no);
-        dto.setWriter(member.get());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        if(!id.equals("anonymousUser"))
+        {
+            Member loginMember=memberRepository.findById(id);
+            dto.setWriter(loginMember);
+            commentService.reply(dto,no);
+            return "redirect:/questions/"+no;
+        }
 
-        commentService.reply(dto,no);
-        return "redirect:/questions/"+no;
+
+      return "redirect:/members/login";
     }
 //selected only one comment
     @PostMapping("comment/selected/{commentNo}")
     public String selected(@PathVariable("commentNo") Long commentNo){
-        Long no = commentService.selected(commentNo);
+       Comment comment=commentRepository.findByCommentNo(commentNo).get();
+        Ask ask=comment.getAsk();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        if(!id.equals("anonymousUser"))
+        {
+            Member loginMember=memberRepository.findById(id);
 
-        return "redirect:/questions/"+no;
+            if(ask.getMember().getNo()==loginMember.getNo())
+            {
+                Long askNo = commentService.selected(commentNo);
+                return "redirect:/questions/"+askNo;
+            }
+            return  "redirect:/questions/"+comment.getAsk().getNo();
+        }
+
+        return "redirect:/members/login";
     }
 //delete parent comment
     @PostMapping("comment/delete/{commentNo}")
     public String delete(@PathVariable("commentNo") Long commentNo){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        if(!id.equals("anonymousUser"))
+        {
+            Member loginMember=memberRepository.findById(id);
+           Comment comment=commentRepository.findByCommentNo(commentNo).get();
+           if(comment.getWriter().getNo()==loginMember.getNo())
+           {
+               Long no = commentService.delete(commentNo);
+               return "redirect:/questions/"+no;
+           }
+           return  "redirect:/questions/"+comment.getAsk().getNo();
+        }
 
 
-        Long no = commentService.delete(commentNo);
-        return "redirect:/questions/"+no;
+        return "redirect:/members/login";
+
+
     }
 
     @PostMapping("/comment/edit/{commentNo}")
     public String edit(CommentRequestDto dto,@PathVariable("commentNo") Long commentNo){
-        Long no = commentService.edit(commentNo, dto);
-        return "redirect:/questions/"+no;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        if(!id.equals("anonymousUser")) {
+            Member loginMember=memberRepository.findById(id);
+
+            if(dto.getWriter().getNo()==loginMember.getNo())
+            {
+                Long no = commentService.edit(commentNo,dto);
+                return "redirect:/questions/"+no;
+            }
+        }
+
+        return "redirect:/members/login";
     }
 
 }
