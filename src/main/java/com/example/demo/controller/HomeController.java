ee1de71ff8f9ff4c5ee6999b36ff00b13001c5c7
younghak9905 <102720771+younghak9905@users.noreply.github.com>
2023-02-28 15:28:30 +0900

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,13 +2,19 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Ask;
 import com.example.demo.domain.Member;
+import com.example.demo.dto.MemberRequestDto;
 import com.example.demo.repository.AskRepository;
 import com.example.demo.repository.CommentRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.AskService;
 import com.example.demo.service.CommentService;
 
+import com.example.demo.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -24,50 +30,75 @@ public class HomeController {
     private final AskRepository askRepository;
     private final MemberRepository memberRepository;
 
+    private final LoginService loginService;
+
+
+
+
+
+
 
     @GetMapping("/")
-    public String home(@SessionAttribute(name = "member", required = false) String member, Model model) {
-        List<Member> members= memberRepository.findAll();
-        int answerCount=0;
-        int score=0;
-        int askCount=0;
-        Member answerKings=null;
-        Member askKings=null;
-        for(Member m:members){
-            int MemberScore= Math.toIntExact(m.getScore());
-            if(score<MemberScore){
-                score=MemberScore;
-                answerKings=m;
+    public String home(Model model ) {
+
+        List<Member> members = memberRepository.findAll();
+        int answerCount = 0;
+        int score = 0;
+        int askCount = 0;
+        Member answerKings = null;
+        Member askKings = null;
+        for (Member m : members) {
+            int MemberScore = Math.toIntExact(m.getScore());
+            if (score < MemberScore) {
+                score = MemberScore;
+                answerKings = m;
             }
         }
-        if(answerKings!=null)
-        {model.addAttribute("answerKings",answerKings);}
+        if (answerKings != null) {
+            model.addAttribute("answerKings", answerKings);
+        }
 
-        for(Member m:members){
-            int MemberAskCount=m.getAsk().size();
+        for (Member m : members) {
+            int MemberAskCount = m.getAsk().size();
 
-            if(MemberAskCount>askCount){
-                askCount=MemberAskCount;
-                askKings=m;
+            if (MemberAskCount > askCount) {
+                askCount = MemberAskCount;
+                askKings = m;
             }
         }
-        if(askKings!=null)
-        {model.addAttribute("askKings",askKings);}
+        if (askKings != null) {
+            model.addAttribute("askKings", askKings);
+        }
+
+
+        model.addAttribute("members", members);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            // authentication 이 존재하면 로그인이 되어있는 상태이므로 home으로 이동
 
 
 
+//authentication 이 존재하면 로그인이 되어있는 상태이므로 home으로 이동
 
 
+            System.out.println("token1: " + authentication.getName());
+            System.out.println("token2: " + SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+            String id = authentication.getName();
+            // token에 저장되어 있는 인증된 사용자의 id 값
+            Member LoginMember = loginService.getUserById(id);
+            MemberRequestDto LoginuserVo = new MemberRequestDto(LoginMember);
+            LoginuserVo.setPassword(null); // password는 보이지 않도록 null로 설정
+            model.addAttribute("LoginMember", LoginuserVo);
 
-        model.addAttribute("members",members);
+            return "loginHome";
+        }
 
        return "home";
     }
 
-    @RequestMapping("/jsp")
-    public String home_jsp() {
-        return "index";
-    }
+
 
     @PostMapping("/search")
     public String search (String search, Model model) {
