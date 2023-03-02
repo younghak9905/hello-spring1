dc4f8c95ad7100932d9c5676d66153e09d56d93e
younghak9905 <102720771+younghak9905@users.noreply.github.com>
2023-03-02 13:52:17 +0900

    Update MemberService.java

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.domain.Ask;
 import com.example.demo.domain.Member;
 import com.example.demo.dto.AskResponseDto;
+import com.example.demo.dto.MemberProfileRequestDto;
 import com.example.demo.dto.MemberRequestDto;
 import com.example.demo.dto.MemberResponseDto;
 import com.example.demo.repository.MemberRepository;
@@ -11,37 +12,33 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 
 import javax.security.auth.login.LoginContext;
+import java.io.Flushable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Transactional
-public class MemberService implements UserDetailsService {
+public class MemberService {
     private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
-  /*  public Long join(Member member) {
-        //같은 id 있는 중복 회원 X
-        validateDuplicateMember(member);
-
-        // Save the member
-        memberRepository.save(member);
-
-        return member.getNo();
-    }*/
 
     public void validateDuplicateMember(Member member) {
-        memberRepository.findById(member.getId())
+        /*memberRepository.findById(member.getId())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
+                });*/
     }
     public Long join(MemberRequestDto requestDto) {
-        validateDuplicateMember(requestDto.toEntity());
+       // validateDuplicateMember(requestDto.toEntity());
 
         return memberRepository.save(requestDto.toEntity()).getNo();
 
@@ -64,23 +61,18 @@ public class MemberService implements UserDetailsService {
         Member member = memberRepository.findByNo(no).get();
         return new MemberResponseDto(member);
     }
-    @Override
-    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
-        Member member = memberRepository.findById(id)
-                .orElseThrow(() -> new UsernameNotFoundException("Can't find"+id));
-        return new UserDetailsImpl(member);
-    }
-    public Member login(String id,String password)
-    {
-        Member member = memberRepository.findById(id)
-                .orElseThrow(() -> new UsernameNotFoundException("Can't find"+id));
-        if(member.getPassword().equals(password))
-        {
-            return member;
+   public Map<String,String> validateHandling(Errors errors){
+        Map<String,String >validatorResult = new HashMap<>();
+        for(FieldError error : errors.getFieldErrors()){
+            String validKeyName = String.format("valid_%s",error.getField());
+            validatorResult.put(validKeyName,error.getDefaultMessage());
         }
-        else
-        {
-            return null;
-        }
-    }
+        return validatorResult;
+   }
+
+
+
+
+
+
 }
